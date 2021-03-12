ed4ad6123fb40f464a7c0202e6ba27d92f17e057
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2021-03-12 16:06:43 +0100

    Artist webhook and project manager

--- models/IdLink.js
+++ models/IdLink.js
@@ -6,6 +6,7 @@ const { conn1 } = require('../index');
 const ProjectSchema = new mongoose.Schema({
 	jiraId: String,
 	discordMessageId: String,
+	type: { type: String, enum: ['translation', 'artist'], required: true },
 });
 
 module.exports = conn1.model('Project', ProjectSchema, 'projects');
