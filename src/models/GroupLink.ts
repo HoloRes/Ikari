04a9f286e835898bfe2bd4e8a7b1d8eac45ecc8d
GoldElysium <goldelysium@gmail.com>
2021-10-08 12:30:39 +0200

    feat(GroupLink): add Discord channel id property

--- src/models/GroupLink.ts
+++ src/models/GroupLink.ts
@@ -5,6 +5,7 @@ import { conn2 } from '../index';
 interface GroupLink {
 	_id: string;
 	jiraName: string;
+	discordChannelId: string;
 	baseRole: boolean;
 }
 
@@ -12,6 +13,7 @@ interface GroupLink {
 const GroupLinkSchema = new mongoose.Schema({
 	_id: { type: String, required: true },
 	jiraName: { type: String, required: true },
+	discordChannelId: { type: string, required: true },
 	baseRole: { type: Boolean, default: false },
 });
 
